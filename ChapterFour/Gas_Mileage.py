0fb3ed7baa10f8dc0249368d837c702110c243b5
blackman147 <chuksntoka@gmail.com>
2021-07-23 09:04:32 +0100

    finished

--- ChapterFour/Gas_Mileage.py
+++ ChapterFour/Gas_Mileage.py
@@ -7,15 +7,18 @@ class Gas_mileage:
     def gas_data(self):
 
         while self.miles_driven != -1 and self.gallons_used != -1:
-            miles_driven = int(input("Enter miles driven: "))
-            gallons_used = int(input("Enter gallons used on this trip"))
+            if self.miles_driven < 0 and self.gallons_used < 0:
+                print("invalid input try again")
 
-            self.miles_per_gallons = miles_driven / gallons_used
+            self.miles_driven = int(input("Enter miles driven or enter -1 to quit: "))
+            self.gallons_used = int(input("Enter gallons used on this trip or enter -1 to quit: "))
+
+            self.miles_per_gallons = self.miles_driven / self.gallons_used
             self.total_miles_per_gallons += self.miles_per_gallons
 
-        while self.miles_driven == -1 and self.gallons_used == -1:
-            print("your miles per gallons is: " + str(self.miles_per_gallons))
-            print("your total miles per gallons is: " + str(self.total_miles_per_gallons))
+            if self.miles_driven == -1 and self.gallons_used == -1:
+                print(" your miles per gallons is: " + str(self.miles_per_gallons))
+                print("your total miles per gallon is: " + str(self.total_miles_per_gallons))
 
 
 def main():
